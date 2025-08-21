8636706c27e5af99d6395f1ae769675f0dab98d4
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-08-21 10:07:02 -0400

    create and add database connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,29 @@
 import express, { type Request, type Response } from 'express';
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 const port = 3001;
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+
+const db = new pg.Client({
+    user: dbUser,
+    host: "localhost",
+    database: "thoughtline",
+    password: dbPass,
+    port: 5433,
+});
+
+try {
+    await db.connect();
+    console.log('Successfully connected to the database!');
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+  }
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
